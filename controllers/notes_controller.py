5e1f2f69d0990f1d4ac9c86d2cf626be83258416
nezirnezirevic310 <1054620@hr.nl>
2023-11-15 13:38:00 +0100

    refactor notes_controller.py

--- controllers/notes_controller.py
+++ controllers/notes_controller.py
@@ -2,17 +2,17 @@ from sqlite3 import Error
 from controllers.database_connector import DatabaseConnector
 
 
-class NotesController:
+class NotesController(DatabaseConnector):
+    database = DatabaseConnector()
+    cursor = database.connect().cursor()
 
     @staticmethod
     def select_notes():
-        database = DatabaseConnector()
-        database.connect()
-        cursor = database.connection.cursor()
+        NotesController.database.connect()
         try:
-            cursor.execute("SELECT * FROM notes")
-            database.connection.commit()
-            notes = cursor.fetchall()
+            NotesController.cursor.execute("SELECT * FROM notes")
+            NotesController.database.connection.commit()
+            notes = NotesController.cursor.fetchall()
             print(notes)
             return notes
         except Error as error:
@@ -20,14 +20,12 @@ class NotesController:
 
     @staticmethod
     def insert_notes(title, note_source, is_public, teacher_id, category_id, note):
-        database = DatabaseConnector()
-        database.connect()
-        cursor = database.connection.cursor()
+        NotesController.database.connect()
         try:
-            cursor.execute("INSERT INTO notes(title, note_source, is_public, teacher_id, category_id, "
-                           "note) VALUES(?, ?, ?, ?, ?, ?)", (title, note_source, is_public, teacher_id,
-                                                              category_id, note))
-            database.connection.commit()
-            return cursor.lastrowid
+            NotesController.cursor.execute("INSERT INTO notes(title, note_source, is_public, teacher_id, category_id, "
+                                           "note) VALUES(?, ?, ?, ?, ?, ?)", (title, note_source, is_public, teacher_id,
+                                                                              category_id, note))
+            NotesController.database.connection.commit()
+            return NotesController.cursor.lastrowid
         except Error as error:
             print(error)
