3d613c021b53069fe84a3e8711c23cf523184793
nezirnezirevic310 <1054620@hr.nl>
2023-11-15 13:50:17 +0100

    refactor database_connector.py

--- controllers/database_connector.py
+++ controllers/database_connector.py
@@ -5,12 +5,13 @@ from sqlite3 import Error
 class DatabaseConnector:
 
     # databases/testgpt.db werkt niet voor mij, verander dit naar je eigen path
-    database_file = "/Users/nezirnezirevic/Desktop/wp2-2023-mvc-1e5-nlbl/databases/testgpt.db"
-    connection = None
+    def __init__(self):
+        self.connection = None
+        self.database_file_path = "databases/testgpt.db"
 
     def connect(self):
         try:
-            self.connection = sqlite3.connect(self.database_file, check_same_thread=False)
+            self.connection = sqlite3.connect(self.database_file_path, check_same_thread=False)
             print("connection success!")
             return self.connection
         except Error as error:
