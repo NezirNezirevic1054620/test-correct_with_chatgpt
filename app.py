8c5e630bb3e0895efcc36cbd33b0ef5a06d48c9a
nezirnezirevic310 <1054620@hr.nl>
2023-11-23 11:35:23 +0100

    fixed create_note.html.j2 form and added views directory

--- app.py
+++ app.py
@@ -1,43 +1,15 @@
 from flask import Flask, render_template
-from controllers.notes_controller import NotesController
-from controllers.categories_controller import CategoriesController
-from flask import request
-from sqlite3 import Error
+
+from views.note import notes_page
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
-
-notes_controller = NotesController()
+app.register_blueprint(notes_page)
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("index.html.j2")
-
-
-@app.route("/create_note", methods=["POST", "GET"])
-def create_note():
-    select_categories = CategoriesController.select_categories()
-    if request.method == "POST":
-        try:
-            title = request.form["title"]
-            note_source = request.form["note_source"]
-            is_public = request.form["is_public"]
-            teacher_id = request.form["teacher_id"]
-            category_id = request.form["category_id"]
-            note = request.form["note"]
-
-            notes_controller.insert_notes(title=title, note_source=note_source, is_public=is_public,
-                                          teacher_id=teacher_id, category_id=category_id, note=note)
-        except Error as error:
-            print(error)
-    return render_template("create_note.html.j2", categories=select_categories)
-
-
-@app.route("/notes")
-def notes():
-    select_notes = NotesController.select_notes()
-    return render_template("notes.html.j2", notes=select_notes)
 
 
 if __name__ == "__main__":
