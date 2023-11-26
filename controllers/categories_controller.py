4bf5e97dc0afe3b1942efbc2b63dc77ab3774e7a
nezirnezirevic310 <1054620@hr.nl>
2023-11-26 14:08:38 +0100

    added update_category

--- controllers/categories_controller.py
+++ controllers/categories_controller.py
@@ -32,8 +32,28 @@ class CategoriesController(DatabaseConnector):
     def delete_category(category_id):
         CategoriesController.database.connect()
         try:
-            CategoriesController.cursor.execute("DELETE FROM categories WHERE category_id="+category_id)
+            CategoriesController.cursor.execute("DELETE FROM categories WHERE category_id=" + category_id)
             CategoriesController.cursor.connection.commit()
         except Error as error:
             print(error)
 
+    @staticmethod
+    def select_category(category_id):
+        CategoriesController.database.connect()
+        try:
+            CategoriesController.cursor.execute("SELECT * FROM categories WHERE category_id=" + category_id)
+            CategoriesController.cursor.connection.commit()
+            return CategoriesController.cursor.fetchall()
+        except Error as error:
+            print(error)
+
+    @staticmethod
+    def update_category(category_id, omschrijving):
+        CategoriesController.database.connect()
+        try:
+            CategoriesController.cursor.execute("UPDATE categories SET omschrijving = '" + omschrijving + "' WHERE " +
+                                                "category_id=" + category_id)
+            CategoriesController.cursor.connection.commit()
+            return CategoriesController.cursor.fetchone()
+        except Error as error:
+            print(error)
