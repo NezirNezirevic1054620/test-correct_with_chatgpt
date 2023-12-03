03c2572231e16d32d1039bc9bad6be4ea841bdd0
nezirnezirevic310 <1054620@hr.nl>
2023-12-03 16:10:39 +0100

    added filter notes

--- controllers/notes_controller.py
+++ controllers/notes_controller.py
@@ -77,3 +77,15 @@ class NotesController(DatabaseConnector):
             return note
         except Error as error:
             print(error)
+
+    @staticmethod
+    def filter_notes(filter_value):
+        NotesController.database.connect()
+        try:
+            NotesController.cursor.execute("SELECT * FROM notes INNER JOIN categories ON notes.category_id = categories.category_id WHERE notes.category_id = " + filter_value)
+            NotesController.cursor.connection.commit()
+            note = NotesController.cursor.fetchall()
+            print(note)
+            return note
+        except Error as error:
+            print(error)
