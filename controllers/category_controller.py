cdbe843d5ff1a858f050b7af6d4f20a215592ede
nezirnezirevic310 <1054620@hr.nl>
2023-12-15 21:00:31 +0100

    added docstrings to category_controller.py and removed linting errors

--- controllers/category_controller.py
+++ controllers/category_controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request, render_template, redirect, url_for, session
+"""Category Controller with all its routes"""
 from sqlite3 import Error
+from flask import Blueprint, request, render_template, redirect, url_for, session
 
 from models.category_model import CategoryModel
 from forms.category_form import CategoryForm
@@ -17,6 +18,7 @@ DATABASE_FILE = "databases/testgpt.db"
 
 @category_page.route("/create_category", methods=["GET", "POST"])
 def create_category():
+    """Create category route that inserts the data from the category form in the database"""
     if "user" in session:
         username = session["user"]
         category_form = CategoryForm()
@@ -25,17 +27,16 @@ def create_category():
             omschrijving = category_form.omschrijving.data
             category_model.insert_category(omschrijving=omschrijving)
             return redirect(url_for("category.categories"))
-        else:
-            print(category_form.errors)
+
         return render_template(
             "category/create_category.html.j2", form=category_form, username=username
         )
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
 
 
 @category_page.route("/")
 def categories():
+    """Categories route that displays all the categories in the database"""
     if "user" in session:
         category_model = CategoryModel(DATABASE_FILE)
         username = session["user"]
@@ -45,12 +46,12 @@ def categories():
             categories=select_categories,
             username=username,
         )
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
 
 
 @category_page.route("/delete_category", methods=["POST", "GET"])
 def delete_category():
+    """Delete category route that deletes a specific categpry from the database"""
     if "user" in session:
         category_model = CategoryModel(DATABASE_FILE)
         if request.method == "POST":
@@ -61,12 +62,12 @@ def delete_category():
             except Error as error:
                 print(error)
         return redirect(url_for("category.categories"))
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
 
 
 @category_page.route("/category/<int:category_id>", methods=["POST", "GET"])
 def category(category_id):
+    """Category route that selects a specific category and displays it in the form"""
     if "user" in session:
         selected_category = None
         category_model = CategoryModel(DATABASE_FILE)
@@ -85,12 +86,12 @@ def category(category_id):
             category_id=category_id,
             username=username,
         )
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
 
 
 @category_page.route("/update_category", methods=["POST", "GET"])
 def update_category():
+    """Update category route that updates the changed fields in the database"""
     if "user" in session:
         username = session["user"]
         category_model = CategoryModel(DATABASE_FILE)
@@ -106,12 +107,13 @@ def update_category():
             except Error as error:
                 print(error)
         return render_template("category/category.html.j2", username=username)
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
 
 
 @category_page.route("/search_category", methods=["POST", "GET"])
 def search_category():
+    """Search category route that searches the category table and displays one or more categories
+    if they align with the search input"""
     if "user" in session:
         result = None
         category_model = CategoryModel(DATABASE_FILE)
@@ -132,5 +134,4 @@ def search_category():
             categories=select_categories,
             username=username,
         )
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
